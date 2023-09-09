abd62e4fe37e04995022bc47747c9eed21eaead8
chanwho <anxi4353@gmail.com>
2023-09-09 13:27:52 +0900

    set three path and rander example1

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -1,6 +1,11 @@
 import * as THREE from "../../node_modules/three/build/three.module.js";
 
+// create scene
 const scene = new THREE.Scene();
+
+//three.perspectiveCamera ( FOV , ratio, near/far cliping plane )
+// FOV : 시야각 :: 볼 수 있는 각도
+// cliping plane : 깎인 평면 :: 랜더링이 될 범위를 결정한다.
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -8,17 +13,20 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
+// webGL : 무료 랜더링 프로그램.
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+const geometry = new THREE.ConeGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 camera.position.z = 5;
 
+// 여기가 랜더링할 수 있는 부분
 function animate() {
   requestAnimationFrame(animate);
 
