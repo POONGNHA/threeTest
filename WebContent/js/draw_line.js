26efc0f2e17d471cde0a9773f1cfaeba89d4af6b
POONGNHA <105198445+POONGNHA@users.noreply.github.com>
2023-09-11 21:21:27 +0900

    draw line

---
+++ WebContent/js/draw_line.js
@@ -0,0 +1,26 @@
+import * as THREE from "../../node_modules/three/build/three.module.js";
+
+const renderer = new THREE.WebGL1Renderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth/window.innerHeight, 1, 500);
+camera.position.set(0,0,100);
+camera.lookAt(0,0,0);
+
+const scene = new THREE.Scene();
+
+// 재질 정하기
+const meterial = new THREE.LineBasicMaterial( { color: 0x0000ff  } );
+
+const points = [];
+points.push( new THREE.Vector3(-10,0,0) );
+points.push( new THREE.Vector3(0,10,0) );
+points.push( new THREE.Vector3(10,0,0) );
+
+const geometry = new THREE.BufferGeometry().setFromPoints( points );
+
+const line = new THREE.Line( geometry, meterial );
+
+scene.add( line );
+renderer.render( scene, camera );
